d2fd2e78a737580fd6d14059bd4018930f807592
Rafael Conrado <52204078+rafael-conrado@users.noreply.github.com>
2022-08-19 13:56:46 -0300

    Develop (#8)
    * feat: rota listar receita por id
    
    * testando checkout para develop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import receitas from './receitasRoutes.js';
 
-
 const routes = (app) => {
     app.route('/').get((req, res) => {
         res.status(200).send({ message: "rota principal ok!" })
