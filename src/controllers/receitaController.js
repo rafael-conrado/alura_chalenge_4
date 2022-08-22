b0a6d60272bdc11b2e48b2d47e445dd15a54e283
Rafael Conrado <rafael070893@hotmail.com>
2022-08-22 15:49:22 -0300

    feat: excluir receita

--- src/controllers/receitaController.js
+++ src/controllers/receitaController.js
@@ -1,10 +1,12 @@
 import receitas from '../models/Receita.js';
+import validacoes from '../middleware/validations.js'
 
 class receitaController {
 
-    static cadastrarReceita(req, res) {
+    static async cadastrarReceita(req, res) {
         let receita = new receitas(req.body);
-
+        let mesAtual = new Date().getMonth() + 1;
+        let anoAtual = new Date().getFullYear();
         const { descricao, valor, data } = req.body;
 
         if (!descricao || !valor || !data) {
@@ -12,15 +14,31 @@ class receitaController {
             return;
         };
 
-        receita.save((error) => {
-            if (error) {
-                res.status(500).send({ message: 'erro ao cadastrar receita', error: error })
-            } else {
-                res.status(201).send({ message: "Receita criada com sucesso!" })
+        receitas.find(
+            {
+                descricao: descricao,
+                data: {
+                    $gte: new Date(`${anoAtual}-${mesAtual}-01`),
+                    $lte: new Date(`${anoAtual}-${mesAtual}-31`)
+                }
+
             }
-        })
+        )
+            .exec((error, result) => {
 
-
+                if (result.length > 0) {
+                    res.status(422).json({ message: "Descrição já cadastrada" });
+                    return;
+                } else {
+                    receita.save((error) => {
+                        if (error) {
+                            res.status(500).send({ message: 'erro ao cadastrar receita', error: error })
+                        } else {
+                            res.status(201).send({ message: "Receita criada com sucesso!" })
+                        }
+                    })
+                }
+            });
     }
 
 
@@ -57,21 +75,46 @@ class receitaController {
 
 
     static atualizarReceita(req, res) {
-        const id = req.params.id;
+        let receita = new receitas(req.body);
+        let mesAtual = new Date().getMonth() + 1;
+        let anoAtual = new Date().getFullYear();
         const { descricao, valor, data } = req.body;
+        const id = req.params.id;
+
 
         if (!descricao || !valor || !data) {
             res.status(422).json({ message: "Campos vazios!" });
             return;
         };
 
-        receitas.findByIdAndUpdate(id, { $set: req.body }, (error) => {
-            if (!error || !receitaAtualizada) {
-                res.status(200).send({ message: "Receita atualizada com sucesso!" })
-            } else {
-                res.status(500).send({ message: "Erro interno!", error: error.message });
+
+        receitas.find(
+            {
+                descricao: descricao,
+                data: {
+                    $gte: new Date(`${anoAtual}-${mesAtual}-01`),
+                    $lte: new Date(`${anoAtual}-${mesAtual}-31`)
+                }
+
             }
-        })
+        )
+            .exec((error, result) => {
+
+                if (result.length > 0) {
+                    res.status(422).json({ message: "Descrição já cadastrada" });
+                    return;
+                } else {
+                    receitas.findByIdAndUpdate(id, { $set: req.body }, (error) => {
+                        if (!error || !receitaAtualizada) {
+                            res.status(200).send({ message: "Receita atualizada com sucesso!" })
+                        } else {
+                            res.status(500).send({ message: "Erro interno!", error: error.message });
+                        }
+                    })
+                }
+            });
+
+
     }
 
     static excluirReceita(req, res) {
@@ -79,7 +122,7 @@ class receitaController {
 
         receitas.findByIdAndDelete(id, (error) => {
             if (!error) {
-                res.status(200).send({ message: "livro removido com sucesso!" });
+                res.status(200).send({ message: "Receita removido com sucesso!" });
             } else {
                 res.status(500).send({ message: error.message })
             }
