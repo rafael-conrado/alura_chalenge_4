e9eec4d82d548b2c8b1d6154a539004cee5448bd
Rafael Conrado <52204078+rafael-conrado@users.noreply.github.com>
2022-08-22 15:53:06 -0300

    Feat excluir receita (#11)
    * feat: rota listar receita por id
    
    * testando checkout para develop
    
    * feat: atualizar receita
    
    * feat: excluir receita
    
    * feat: excluir receita

--- src/routes/receitasRoutes.js
+++ src/routes/receitasRoutes.js
@@ -7,6 +7,8 @@ router
     .get('/receitas', receitasController.listarTodasReceitas)
     .get('/receitas/:id', receitasController.listarReceitaPorId)
     .post('/receitas', receitasController.cadastrarReceita)
+    .put('/receitas/:id',receitasController.atualizarReceita)
+    .delete('/receitas/:id',receitasController.excluirReceita)
 
 
 export default router;
